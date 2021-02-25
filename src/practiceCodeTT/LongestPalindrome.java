c064eba52ad101a5fef7bdc01548626a9c1c6a4d
saku.isaket <saku.isaket@LAPTOP-S2GMU1FV>
2021-02-26 02:49:56 +0530

    ~ code checkin ~

---
+++ src/practiceCodeTT/LongestPalindrome.java
@@ -0,0 +1,18 @@
+package practiceCodeTT;
+
+/**
+ * @author saku.isaket
+ * this program is to check the longest palindrome in the string.
+ *  Ex : abcba
+ */
+public class LongestPalindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("run");
+
+	}
+
+}
