1f2d5ed1e8444519de250570a813a14acc9ee0e7
saku.isaket <saku.isaket@LAPTOP-S2GMU1FV>
2021-03-01 02:42:08 +0530

    ~ code to count repetition of chars in string ~

---
+++ src/practiceCodeTT/CountCharacters.java
@@ -0,0 +1,36 @@
+package practiceCodeTT;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author saku.isaket
+ * to count number of chars repeated in the string using hash map
+ */
+public class CountCharacters {
+
+		public void countChars(String message){
+			Map<Character, Integer> numCharMap = new HashMap<Character, Integer>();
+			for(int i = 0; i < message.length(); i++){
+				char c = message.charAt(i);
+				if(c == ' ')
+					continue;
+					if(numCharMap.containsKey(c)){
+						numCharMap.put(c, numCharMap.get(c)+1);
+					}else{
+						numCharMap.put(c, 1);
+					}
+			}
+			Set<Map.Entry<Character, Integer>> numSet = numCharMap.entrySet();
+			for(Map.Entry<Character, Integer> m : numSet){
+				System.out.println("Char- "+m.getKey()+" Count- "+m.getValue());
+			}
+		}		
+		public static void main(String[] args) {
+		    CountCharacters cc = new CountCharacters();
+		    cc.countChars("Saket Kumar");
+		  
+	}
+
+}
